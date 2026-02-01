a5d096d231b91a97acb46beb613a7207a502e017
sivaprasath-2002 <sivaprasath.a.2002@gmail.com>
2026-02-01 10:49:08 +0530

    code was changes to previous of java which includes public class and args array in main function (It Shows 3 warnings)

--- src/Odd_Even.java
+++ src/Odd_Even.java
@@ -1,14 +1,18 @@
-void main() {
-    Scanner in = new Scanner(System.in);
-    IO.print("Enter a Number: ");
-    int num = in.nextInt();
-    oddOrEven(num);
-}
+import java.util.Scanner;
+public class Odd_Even {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a Number: ");
+        int num = in.nextInt();
+        oddOrEven(num);
+    }
 
-static void oddOrEven(int num) {
-    if (num % 2 == 0) {
-        IO.println(num + " is an Even number. ");
-    } else {
-        IO.println(num + " is an Odd number. ");
+    static void oddOrEven(int num){
+        if (num % 2 == 0){
+            System.out.println(num + " is an Even number. ");
+        }
+        else{
+            System.out.println(num+ " is an Odd number. ");
+        }
     }
 }
